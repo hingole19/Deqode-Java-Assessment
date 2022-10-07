f4e47fbb91fba37a5fc791895aa7b3ae22fce4cd
harishingole <harishingole19@gmail.com>
2022-10-07 09:51:20 +0530

    springpracticewithnosql

---
+++ src/main/java/deqode/springpractice/backend2/SpringpracticeApplication.java
@@ -0,0 +1,13 @@
+package deqode.springpractice.backend2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/deqode/springpractice/com/SpringpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package deqode.springpractice.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
